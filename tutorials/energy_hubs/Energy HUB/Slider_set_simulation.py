a0f47080bdab91a42458e2207fa5b3722bff07b0
Dialectiek <dsp.georgiadi@gmail.com>
2026-01-16 16:18:10 +0100

    added missing monitor items, fixed min & max p calculation, added cells for debugging...

--- tutorials/energy_hubs/Energy HUB/Slider_set_simulation.py
+++ tutorials/energy_hubs/Energy HUB/Slider_set_simulation.py
@@ -13,34 +13,34 @@ def run_res_simulation(dashboard):
     storage_slider  = dashboard["storage_slider"]
     p_rated_turbine = dashboard["p_rated_turbine"]
 
-    CONFIG_FILE = 'data\Sliders_Scenario_grid.yaml'
+    CONFIG_FILE = 'data\Tutorial_physical_congestion_RES_bat.yaml'
     simulation_RES = Simulation(CONFIG_FILE)
 
-    load_files = {
-        "Rotterdam": "data\load_data.txt",
-        "Netherlands": "nl_entsoe_demand_household_cleaned.csv",
-    }
+    # load_files = {
+    #     "Rotterdam": "data\load_data.txt",
+    #     "Netherlands": "nl_entsoe_demand_household_cleaned.csv",
+    # }
 
-    pv_files = {
-        "Rotterdam": "data\pv_data_Rotterdam_NL-15min.txt",
-        "Netherlands": "pv_data_Rotterdam_NL-15min_2025.txt",
-    }
+    # pv_files = {
+    #     "Rotterdam": "data\pv_data_Rotterdam_NL-15min.txt",
+    #     "Netherlands": "pv_data_Rotterdam_NL-15min_2025.txt",
+    # }
 
-    wind_files = {
-        "Rotterdam": "data\winddata_NL.txt",
-        "Netherlands": "winddata_NL_2025.txt",
-    }
+    # wind_files = {
+    #     "Rotterdam": "data\winddata_NL.txt",
+    #     "Netherlands": "winddata_NL_2025.txt",
+    # }
 
-    times = {
-        "Rotterdam": {
-            'start_time': '2012-02-01 00:00:00',
-            'end_time': '2012-02-01 23:45:00'
-        },
-        "Netherlands": {
-            'start_time': '2025-11-28 00:00:00',
-            'end_time': '2025-12-02 23:45:00'
-        }
-    }
+    # times = {
+    #     "Rotterdam": {
+    #         'start_time': '2012-02-01 00:00:00',
+    #         'end_time': '2012-02-01 23:45:00'
+    #     },
+    #     "Netherlands": {
+    #         'start_time': '2025-11-28 00:00:00',
+    #         'end_time': '2025-12-02 23:45:00'
+    #     }
+    # }
 
     def int_to_vector(n, length=10):
         return [1]*n + [0]*(length-n)
@@ -48,24 +48,24 @@ def run_res_simulation(dashboard):
     def apply_ui_to_simulation():
         selected_dataset = dropdown.value
 
-        simulation_RES.set_model_param(
-            model_name='CSVload',
-            parameter='file_path',
-            value=load_files[selected_dataset]
-        )
-        simulation_RES.set_model_param(
-            model_name='CSV_pv',
-            parameter='file_path',
-            value=pv_files[selected_dataset]
-        )
-        simulation_RES.set_model_param(
-            model_name='CSV_wind',
-            parameter='file_path',
-            value=wind_files[selected_dataset]
-        )
+        # simulation_RES.set_model_param(
+        #     model_name='CSVload',
+        #     parameter='file_path',
+        #     value=load_files[selected_dataset]
+        # )
+        # simulation_RES.set_model_param(
+        #     model_name='CSV_pv',
+        #     parameter='file_path',
+        #     value=pv_files[selected_dataset]
+        # )
+        # simulation_RES.set_model_param(
+        #     model_name='CSV_wind',
+        #     parameter='file_path',
+        #     value=wind_files[selected_dataset]
+        # )
 
-        simulation_RES.set_scenario_param('start_time', times[selected_dataset]['start_time'])
-        simulation_RES.set_scenario_param('end_time', times[selected_dataset]['end_time'])
+        # simulation_RES.set_scenario_param('start_time', times[selected_dataset]['start_time'])
+        # simulation_RES.set_scenario_param('end_time', times[selected_dataset]['end_time'])
 
         pv_number     = pv_size_slider.value
         pv_size_kW    = pv_number * 0.5  # kW
@@ -75,11 +75,12 @@ def run_res_simulation(dashboard):
         Turbines_on = n_turbines
 
         new_settings = {
+
             'Wind1':  {'p_rated': n_turbines},
             'PV1':     {'cap': pv_size_kW},
             'Battery1': {
-                'max_p':      storage_level*0.9,
-                'min_p':     -storage_level*0.9,
+                'max_p':      storage_level*0.1,
+                'min_p':      storage_level*0.1,
                 'max_energy': storage_level
             },
             'Controller1': {
@@ -101,7 +102,7 @@ def run_res_simulation(dashboard):
         return new_settings
 
     new_settings = apply_ui_to_simulation()
-    simulation_RES.edit_models(new_settings)
+    # simulation_RES.edit_models(new_settings)
 
     csvload_model = [m for m in simulation_RES.config['models'] if m['name'] == 'CSVload'][0]
     path = csvload_model['parameters']['file_path']
@@ -113,6 +114,6 @@ def run_res_simulation(dashboard):
     for c in df_load.columns:
         print("  ", repr(c))
 
-    simulation_RES.run()
+    # simulation_RES.run()
 
     return simulation_RES, new_settings, df_load
