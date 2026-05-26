ce62e0981a4b1b64ec81ae1a117599a6253422d9
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2026-05-26 16:55:02 +0200

    Update datetime parsing format in plot_results_connection

--- tutorials/physical_congestion/functions_T3.py
+++ tutorials/physical_congestion/functions_T3.py
@@ -36,7 +36,7 @@ def plot_load_on_connection(load_df, day, number_houses, tolerance_limit, critic
 def plot_results_connection(results_file, connection_cap, critical_limit, tolerance_limit):
 
     result_pd_df = pd.read_csv(results_file, index_col=0)
-    result_pd_df.index = pd.to_datetime(result_pd_df.index)
+    result_pd_df.index = pd.to_datetime(result_pd_df.index, format="mixed")
     load_on_connection = -result_pd_df['Controller1.dump']
 
     connection_cap = connection_cap
